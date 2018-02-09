a658391aa467edf5d62a717c98264b0d6829978e
Ginger Lee Kretschmer <kretschmons@gmail.com>
2018-02-09 14:18:24 -0800

    add in looping for keg items onto keglist component

---
+++ src/components/KegItem.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function KegItem(props) {
+  return (
+    <div>
+      <h3>{props.name}</h3>
+      <h3>{props.brand}</h3>
+      <p>{props.price}</p>
+      <p>{props.abv}</p>
+    </div>
+  );
+}
+
+KegItem.propTypes = {
+  name: PropTypes.string,
+  brand: PropTypes.string,
+  price: PropTypes.number,
+  abv: PropTypes.string
+};
+
+export default KegItem;
--- src/containers/KegList.jsx
+++ src/containers/KegList.jsx
@@ -1,9 +1,45 @@
 import React from 'react';
+import KegItem from '../components/KegItem.jsx';
+
+const masterKegList = [
+  {
+    name: 'GingaNinja',
+    brand: 'Two Towns Cider',
+    price: 4.50,
+    abv: '5.2%'
+  },
+  {
+    name: 'Black (黑)',
+    brand: 'Mikkeller',
+    price: 9.00,
+    abv: '17.5%'
+  },
+  {
+    name: 'Knights of Chris-Lumbus',
+    brand: 'Geno-Ciders',
+    price: 6.50,
+    abv: '7.5%'
+  },
+  {
+    name: 'Over Hopulation',
+    brand: 'Barly Brown\'s Beer',
+    price: 6.00,
+    abv: '8.5%'
+  }
+];
 
 function KegList() {
   return (
     <div>
       <h1>Daily Taps</h1>
+      {masterKegList.map((keg, index) =>
+        <KegItem
+          name={keg.name}
+          brand={keg.brand}
+          price={keg.price}
+          abv={keg.abv}
+          key={index}/>
+      )}
     </div>
   );
 }
