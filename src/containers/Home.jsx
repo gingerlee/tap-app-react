3e819e37f26b4f9985c6a836c535ceeaad0e5d63
Ginger Lee Kretschmer <kretschmons@gmail.com>
2018-02-09 13:40:28 -0800

    add keglist component to homepage

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Nav from '../components/Nav';
 import Hero from '../components/Hero';
 import homeBeer from '../assets/images/home-beer.jpg';
+import KegList from './KegList';
 
 function Home(){
   return(
@@ -10,6 +11,7 @@ function Home(){
         heroTitle={'Tap That App'}
         heroImage={homeBeer}/>
       <Nav/>
+      <KegList/>
     </div>
   );
 }
---
+++ src/containers/KegList.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function KegList() {
+  return (
+    <div>
+      <h1>Daily Taps</h1>
+    </div>
+  );
+}
+export default KegList;
