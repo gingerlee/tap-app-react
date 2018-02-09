0fa39679a943904fdcc56f0909150a4019c5aabe
Ginger Lee Kretschmer <kretschmons@gmail.com>
2018-02-09 14:35:49 -0800

    add nav styling

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,14 +2,24 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Nav() {
+  const linkDecoration = {
+    textDecoration: 'none',
+    color: '#000',
+    margin: '10px'
+  };
   return(
     <div>
-      <Link to='/'><a>Home</a></Link> |
-      <Link to='/admin'><a>Admin</a></Link>
+      <Link style={linkDecoration} to='/'><a>Home</a></Link>
+      <Link style={linkDecoration} to='/admin'><a> Admin</a></Link>
       <style jsx>{`
-          a {
-            text-decoration: none;
-            color: #000;
+          a:hover {
+            opacity: .5;
+          }
+          div {
+            display: flex;
+            justify-content: center;
+            padding: 2em;
+
           }
 
         `}</style>
